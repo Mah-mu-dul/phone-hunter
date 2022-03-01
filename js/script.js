73bb7625c0e7fda29f0b58a4a03c929b3fa7f5c7
habituatedcoder <work.mahmudulhasan@gmail.com>
2022-03-01 11:38:16 +0600

    search box added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('kicu ekta');
+const phoneSearch = () =>{
+    fetch('https://openapi.programming-hero.com/api/phones?search=samsung')
+    .then(res => res.json())
+    .then(data => brand(data))
+}   
+phoneSearch()
+
+const brand = (data) =>{
+    console.log(data.data[0].brand);
+}
+
+
+const searchValue = document.getElementById('searchBox').value 
+const searchBtn = document.getElementById('searchBtn') 
+searchBtn.addEventListener('click', function(){
+    const searchValue = document.getElementById('searchBox').value 
+    console.log( searchValue)
+})
